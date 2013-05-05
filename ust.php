4034151280aa74ddee9954fb522863c8f3499fc3
Mehmet Seçkin <info@mehmetseckin.com>
2013-05-05 23:16:00 +0300

    Background
    Added background psd and png's.

--- ust.php
+++ ust.php
@@ -1,3 +1,9 @@
-<?php
-
-?>
+<?php include "ayarlar.php"; ?>
+<html>
+<head>
+<title><?php if (isset($_SESSION['title'])) echo $_SESSION['title']; else echo "Samymy!"; ?></title>
+<link rel="stylesheet" href="css/main.css"
+</head>
+<body>
+<div id="background-left"></div>
+<div id="background-right"></div>
