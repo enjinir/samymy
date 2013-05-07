c022299d634a276d8d9a6de0dd0014025307f9fc
Çağlayan Ece Ünal <caglayan.ece.unal@gmail.com>
2013-05-07 23:23:27 +0300

    CSS Overflow sorunu düzeltildi.

--- hakkimizda.php
+++ hakkimizda.php
@@ -1,13 +1,24 @@
 <?php
-
-
 // Ust kisim
 include "ust.php";
+  ?>
+   <img src= "resimler\urunler\kiz_cocuk3.jpg" width="480" height="360">
+  <div id="hakkimizda">
+   <br><br>
    
-   echo "En özel günlerinizde sizi yalnız bırakmayan değerli dostlarınıza,ailenize ve tüm sevdiklerinize güzel bir jest amaçlı ufak hediyeler sunmanız 
-   için kurulan sAMYMy  çeşitli ürünleriyle ve uygun fiyatlarıyla sizlerle.. Gerek sitedeki ürünlerden gerekse istediğiniz herhangi bir model siparişinizle
-   bu güzel sabunlara sahip olabilirsiniz. ";
+   <p><br>
+     En özel günlerinizde sizi yalnız bırakmayan değerli dostlarınıza,ailenize ve tüm sevdiklerinize güzel bir jest amaçlı ufak hediyeler sunmanız 
+   için kurulan sAMYMy  çeşitli ürünleriyle ve uygun fiyatlarıyla sizlerle.. 
+   </p>
+   <br>
+   <p>
+     Gerek sitedeki ürünlerden gerekse istediğiniz herhangi bir model siparişinizle
+   bu güzel sabunlara sahip olabilirsiniz. 
+	</p>
 
+  </div>
+ 	
+<?php
 // Alt kisim
 include "alt.php";
 
