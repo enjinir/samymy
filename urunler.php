3d06ae71076f3e5effd512e7aae0dbb4a055a911
Mehmet Seçkin <info@mehmetseckin.com>
2013-05-08 04:23:50 +0300

    Urunler

--- urunler.php
+++ urunler.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
 $_SESSION["title"] = "Samymy : Ürünler"; 
-
 // Ust kisim
 include "ust.php";
-
+if(isset($_GET["kategori"])) $kategori = $_GET["kategori"]; else $kategori = 1;
+echo '<div id="urunler">';
+$sorgu = "SELECT * FROM urunler WHERE kategori_id=$kategori";
+$sonuc = mysql_query($sorgu);
+while($urun = mysql_fetch_array($sonuc)) {
+?>
+<div class="urun">
+	<img class="resim" src="<?php echo "resimler/urunler/".$urun["resim"]; ?>" />
+	<span class="ad"><?php echo $urun["ad"]; ?></span>
+</div>
+<?php
+}
+echo '</div>';
 // Alt kisim
 include "alt.php";
 
