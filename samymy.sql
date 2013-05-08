2995677051ae297d7bbfdaf66a3c8957eb3122e3
Mehmet Seçkin <info@mehmetseckin.com>
2013-05-08 04:55:44 +0300

    Newly

--- samymy.sql
+++ samymy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.5.5
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: May 06, 2013 at 07:24 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Anamakine: localhost
+-- Üretim Zamanı: 08 May 2013, 04:55:21
+-- Sunucu sürümü: 5.1.68-cll
+-- PHP Sürümü: 5.3.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,33 +17,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `samymy`
+-- Veritabanı: `samymy_main`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategoriler`
+-- Tablo için tablo yapısı `kategoriler`
 --
 
 CREATE TABLE IF NOT EXISTS `kategoriler` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ad` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin5 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin5 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `kategoriler`
+-- Tablo döküm verisi `kategoriler`
 --
 
 INSERT INTO `kategoriler` (`id`, `ad`) VALUES
 (1, 'Kız Bebeklere Özel Ürünler'),
-(2, 'Erkek Bebeklere Özel Ürünler');
+(2, 'Erkek Bebeklere Özel Ürünler'),
+(3, 'Doğumgünü Ürünleri');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kullanicilar`
+-- Tablo için tablo yapısı `kullanicilar`
 --
 
 CREATE TABLE IF NOT EXISTS `kullanicilar` (
@@ -60,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `kullanicilar` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `siparisler`
+-- Tablo için tablo yapısı `siparisler`
 --
 
 CREATE TABLE IF NOT EXISTS `siparisler` (
@@ -75,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `siparisler` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `urunler`
+-- Tablo için tablo yapısı `urunler`
 --
 
 CREATE TABLE IF NOT EXISTS `urunler` (
@@ -84,12 +85,23 @@ CREATE TABLE IF NOT EXISTS `urunler` (
   `kategori_id` int(11) NOT NULL,
   `resim` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin5 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin5 AUTO_INCREMENT=9 ;
+
+--
+-- Tablo döküm verisi `urunler`
+--
+
+INSERT INTO `urunler` (`id`, `ad`, `kategori_id`, `resim`) VALUES
+(3, 'Deneme ürün 1', 2, ''),
+(4, 'Deneme ürün 2', 2, 'erkek_cocuk1.jpg'),
+(5, 'Deneme ürün3', 3, 'ugur_bocegi.jpg'),
+(6, 'Deneme ürün 5', 1, 'erkek_cocuk5.jpg'),
+(7, 'Deneme ürün 6', 1, 'kız_cocuk 1.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `yoneticiler`
+-- Tablo için tablo yapısı `yoneticiler`
 --
 
 CREATE TABLE IF NOT EXISTS `yoneticiler` (
@@ -100,7 +112,15 @@ CREATE TABLE IF NOT EXISTS `yoneticiler` (
   `kullanici_adi` varchar(16) NOT NULL,
   `sifre` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin5 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin5 AUTO_INCREMENT=3 ;
+
+--
+-- Tablo döküm verisi `yoneticiler`
+--
+
+INSERT INTO `yoneticiler` (`id`, `ad`, `soyad`, `email`, `kullanici_adi`, `sifre`) VALUES
+(1, 'Mehmet', 'Seçkin', 'seckin92@gmail.com', 'seckin92', '2d9b3ad3b32e0075504a94e2965fc764'),
+(2, 'Çağlayan Ece', 'Ünal', 'caglayan.ece.unal@gmail.com', 'caglayanece', '425b23ef9fadc6224d38b1402e5b3aad');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- urunler.php
+++ urunler.php
@@ -3,9 +3,10 @@ session_start();
 $_SESSION["title"] = "Samymy : Ürünler"; 
 // Ust kisim
 include "ust.php";
-if(isset($_GET["kategori"])) $kategori = $_GET["kategori"]; else $kategori = 1;
+if(isset($_GET["kategori"])) $kategori = $_GET["kategori"]; else $kategori = -1;
 echo '<div id="urunler">';
-$sorgu = "SELECT * FROM urunler WHERE kategori_id=$kategori";
+$sorgu = "SELECT * FROM urunler";
+if($kategori > 0) $sorgu .= " WHERE kategori_id=$kategori";
 $sonuc = mysql_query($sorgu);
 while($urun = mysql_fetch_array($sonuc)) {
 ?>
